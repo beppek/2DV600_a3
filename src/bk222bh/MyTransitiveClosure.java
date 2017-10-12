1671c1b9bbaab03bd3f6bfeefd2ff428a65070cf
Beppe Karlsson <beppe@live.se>
2017-10-12 21:13:33 +0200

    simple transitive closure implementation

--- src/bk222bh/MyTransitiveClosure.java
+++ src/bk222bh/MyTransitiveClosure.java
@@ -1,15 +1,22 @@
 package bk222bh;
 
+import graphs.DFS;
 import graphs.DirectedGraph;
 import graphs.Node;
 import graphs.TransitiveClosure;
 
-import java.util.Collection;
-import java.util.Map;
+import java.util.*;
 
 public class MyTransitiveClosure<E> implements TransitiveClosure {
     @Override
-    public Map<Node, Collection<Node>> computeClosure(DirectedGraph dg) {
-        return null;
+    public Map<Node<E>, Collection<Node<E>>> computeClosure(DirectedGraph dg) {
+        Map<Node<E>, Collection<Node<E>>> map = new HashMap();
+        Iterator<Node<E>> it = dg.iterator();
+        DFS dfs = new MyDFS<Node<E>>();
+        while (it.hasNext()) {
+            Node<E> node = it.next();
+            map.put(node, dfs.dfs(dg, node));
+        }
+        return map;
     }
 }
